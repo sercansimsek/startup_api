5067d5599cec9ca5e9d9f694c274093dba9b7e5f
Sercan <sercansimsek44@gmail.com>
2026-08-10 12:51:52 +0200

    Serving data

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 import express from "express";
+import { startups } from "./data/data.js";
 
 const PORT = 8000;
 
 const app = express();
+
+app.get("/api", (req, res) => {
+  res.json(startups);
+});
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
