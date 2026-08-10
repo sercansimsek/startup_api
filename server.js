db3bdda3b395e70ce748ffd1de9460feae1d5160
Sercan <sercansimsek44@gmail.com>
2026-08-10 14:51:16 +0200

    allowed path parameters declared

--- server.js
+++ server.js
@@ -49,6 +49,17 @@ app.get("/api", (req, res) => {
 app.get("/api/:field/:term", (req, res) => {
   const { field, term } = req.params;
 
+  const allowedFields = ["country", "continent", "industry"];
+
+  if (!allowedFields.includes(field.toLowerCase())) {
+    const errMsg = {
+      message:
+        "Search field not allowed. Please use only 'country', 'continent', 'industry'",
+    };
+    res.statusCode = 400;
+    return res.json(errMsg);
+  }
+
   const filteredData = startups.filter((startup) => {
     return startup[field].toLowerCase() === term.toLowerCase();
   });
