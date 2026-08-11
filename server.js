5fc9a710b685773c805948b3d176e0d1bbb59add
Sercan <sercansimsek44@gmail.com>
2026-08-11 09:30:41 +0200

    Error handle for not existing endpoint

--- server.js
+++ server.js
@@ -6,5 +6,12 @@ const PORT = 8000;
 const app = express();
 
 app.use("/api", apiRouter);
+app.use((req, res) => {
+  const errMsg = {
+    message: "Endpoint not found. Please check the API documentation.",
+  };
+  res.statusCode = 404;
+  return res.json(errMsg);
+});
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
