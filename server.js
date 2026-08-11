23ed9961c6d6e1bd4de9eaa033315176c0f09dc8
Sercan <sercansimsek44@gmail.com>
2026-08-11 09:35:38 +0200

    CORS

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { apiRouter } from "./routes/apiRoutes.js";
+import cors from "cors";
 
 const PORT = 8000;
 
 const app = express();
 
+app.use(cors());
 app.use("/api", apiRouter);
 app.use((req, res) => {
   const errMsg = {
